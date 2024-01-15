7b8a333b2987ce40faf26845a2148d628f467293
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2024-01-15 22:13:39 +0100

    Tilt hero sprite

--- badge/playing_state.py
+++ badge/playing_state.py
@@ -4,10 +4,9 @@ from state import State
 from sprites import sprites
 import constants
 
-hero_width = 9
-hero_height = 9
-hero_max_x = constants.SCREEN_WIDTH - hero_width
-hero_max_y = constants.SCREEN_HEIGHT - hero_height
+hero_max_x = constants.SCREEN_WIDTH - constants.HERO_WIDTH
+hero_max_y = constants.SCREEN_HEIGHT - constants.HERO_HEIGHT
+sprite_tilt_acc = 0.7
 
 # Screen width is 28 mm, resolution 128, i.e.:
 px_per_m = 4571
@@ -39,6 +38,12 @@ class PlayingState(State):
         machine.display.show(self.root_group)
 
     def update(self, machine):
+        self.update_positition(machine)
+        self.update_sprite(machine)
+        machine.display.refresh()
+
+
+    def update_positition(self, machine):
         input = machine.input
         time_diff = machine.time_diff
         machine.vel_x += input.acc_x * time_diff * physics_scale
@@ -50,9 +55,9 @@ class PlayingState(State):
         nx_tmp = machine.pos_x + dx
         ny_tmp = machine.pos_y + dy
 
-        if nx_tmp < 0 or nx_tmp > 128 - hero_width:
+        if nx_tmp < 0 or nx_tmp > 128 - constants.HERO_WIDTH:
             machine.vel_x *= -bounce_factor
-        if ny_tmp < 0 or ny_tmp > 160 - hero_height:
+        if ny_tmp < 0 or ny_tmp > 160 - constants.HERO_HEIGHT:
             machine.vel_y *= -bounce_factor
 
         machine.pos_x = max(min(nx_tmp, hero_max_x), 0)
@@ -60,4 +65,21 @@ class PlayingState(State):
 
         self.hero_group.x = int(machine.pos_x)
         self.hero_group.y = int(machine.pos_y)
-        machine.display.refresh()
+
+
+    def update_sprite(self, machine):
+        input = machine.input
+        if input.acc_x < -sprite_tilt_acc:
+            x = 0
+        elif input.acc_x > sprite_tilt_acc:
+            x = 2
+        else:
+            x = 1
+        if input.acc_y < -sprite_tilt_acc:
+            y = 0
+        elif input.acc_y > sprite_tilt_acc:
+            y = 2
+        else:
+            y = 1
+        i = 3 * y + x
+        self.hero_sprite[0] = i
--- badge/sprites.py
+++ badge/sprites.py
@@ -1,14 +1,21 @@
 import displayio
+import constants
 
-def load_sprite(name, transparency=True):
+def load_sprite(name, width, height, tile_width, tile_height, default_tile, transparency=True):
     bitmap = displayio.OnDiskBitmap('/assets/' + name + '.bmp')
     if transparency:
         bitmap.pixel_shader.make_transparent(0)
-    sprite = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader)
+    sprite = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader,
+        width=1, height=1, tile_width=constants.HERO_WIDTH, tile_height=constants.HERO_HEIGHT,
+        default_tile=4)
     return sprite
 
 
 sprites = {}
 
 def load_all():
-    sprites['hero'] = load_sprite('hero')
+    sprites['hero'] = load_sprite(
+        'hero',
+        width=1, height=1,
+        tile_width=constants.HERO_WIDTH, tile_height=constants.HERO_HEIGHT,
+        default_tile=4)
