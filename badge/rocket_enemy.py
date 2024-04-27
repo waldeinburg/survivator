5416448b3778d8e7bf6e22be76215c5479fae8c4
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2024-04-27 18:13:50 +0200

    Rocket enemy can hit and be hit

--- badge/rocket_enemy.py
+++ badge/rocket_enemy.py
@@ -6,7 +6,8 @@ import constants
 from util import LEFT, RIGHT, UP, DOWN, get_hero_center, get_direction_to_hero
 
 radius = constants.ROCKET_SIZE / 2
-
+hit_distance = (constants.HERO_SIZE + constants.ROCKET_SIZE) / 2
+destroy_distance = constants.WEAPON_RADIUS + radius
 
 class RocketEnemy(Enemy):
 
@@ -20,31 +21,31 @@ class RocketEnemy(Enemy):
 
         self.hit = False
         self.destroyed = False
-        self.acceleration = 10
-        self.init_speed = 50
-        self.max_speed = 100
+        self.acceleration = 2500
+        self.init_speed = 80
+        self.max_speed = self.init_speed
 
         self.sprite = sprites['rocket']
         self.sprite[0] = 0
 
         if side == LEFT or side == RIGHT:
-            self.y = start_y
+            self.cy = start_y
             if side == LEFT:
-                self.x = -radius
+                self.cx = -radius
             else:
-                self.x = constants.PLAY_WIDTH + radius
+                self.cx = constants.PLAY_WIDTH + radius
         else:
-            self.x = start_x
+            self.cx = start_x
             if side == UP:
-                self.y = -radius
+                self.cy = -radius
             else:
-                self.y = constants.PLAY_HEIGHT + radius
-        self.group.x = round(self.x - radius)
-        self.group.y = round(self.y - radius)
+                self.cy = constants.PLAY_HEIGHT + radius
+        self.group.x = round(self.cx - radius)
+        self.group.y = round(self.cy - radius)
 
         self.group.append(self.sprite)
 
-        self.vel_x, self.vel_y, *_ = get_direction_to_hero(self.x, self.y, self.init_speed, machine)
+        self.vel_x, self.vel_y, *_ = get_direction_to_hero(self.cx, self.cy, self.init_speed, machine)
 
 
     def destroy(self, machine):
@@ -52,21 +53,27 @@ class RocketEnemy(Enemy):
         machine.rocket = False
 
 
+    def distance_to_hero(self, machine):
+        hx, hy = get_hero_center(machine)
+        return math.sqrt((hx - self.cx)**2 + (hy - self.cy)**2)
+
+
     def update_enemy(self, machine):
-        if machine.weapon_active:
+        if machine.weapon_active and self.distance_to_hero(machine) < destroy_distance:
             self.destroyed = True
         if self.destroyed:
             return
         time_diff_sec = machine.time_diff / 1000
-        acc_x, acc_y, *_ = get_direction_to_hero(self.x, self.y, self.acceleration, machine)
-        self.vel_x += acc_x * time_diff_sec
-        self.vel_y += acc_y * time_diff_sec
+        if not self.hit:
+            acc_x, acc_y, *_ = get_direction_to_hero(self.cx, self.cy, self.acceleration, machine)
+            self.vel_x += acc_x * time_diff_sec
+            self.vel_y += acc_y * time_diff_sec
         self.vel_x = min(self.vel_x, self.max_speed)
         self.vel_y = min(self.vel_y, self.max_speed)
-        self.x += self.vel_x * time_diff_sec
-        self.y += self.vel_y * time_diff_sec
-        self.group.x = round(self.x - radius)
-        self.group.y = round(self.y - radius)
+        self.cx += self.vel_x * time_diff_sec
+        self.cy += self.vel_y * time_diff_sec
+        self.group.x = round(self.cx - radius)
+        self.group.y = round(self.cy - radius)
 
 
     def has_hit(self, machine):
@@ -74,3 +81,7 @@ class RocketEnemy(Enemy):
             return False
         if self.hit:
             return True
+        hx, hy = get_hero_center(machine)
+        if self.distance_to_hero(machine) < hit_distance:
+            self.hit = True
+        return self.hit
